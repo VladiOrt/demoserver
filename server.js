d3890f63a6ebf29c84c5c90bbc37265046b3ce66
Vladi Ortega <vladi_ortega@hotmail.com>
2022-09-23 14:24:58 -0500

    update new campos

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
+var moment = require('moment'); // require
+
 const express = require('express');
 const cors = require('cors');
 const {json} =  require('express');
 const routes = require('./routes')
 var mysql = require('mysql');
+
+var cron = require('node-cron');
 
 
 const funciones = require('./models/index')
@@ -28,90 +32,6 @@ app.use(express.urlencoded({extended:true}))
 
 
 
-
-
-
-
-
-/*
-
-
-
-
-
-let crearTablaSucursal = "CREATE TABLE IF NOT EXISTS Sucursal ("+
-    "idSucursal INT AUTO_INCREMENT PRIMARY KEY,"+
-    "nombre VARCHAR(255) NOT NULL,"+
-    "telefono VARCHAR(255) NOT NULL,"+
-    "horario VARCHAR(255) NOT NULL,"+
-    "marca VARCHAR(255) NOT NULL,"+
-    "nombrePlaza VARCHAR(255) NOT NULL,"+
-    "direccionPlaza VARCHAR(255) NOT NULL,"+
-    "encargadoPlaza VARCHAR(255) NOT NULL,"+
-    "numeroCabinas VARCHAR(255) NOT NULL,"+
-    "created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP)" 
-
-let crearTablaPuest = "CREATE TABLE IF NOT EXISTS puesto ("+
-    "idPuesto INT AUTO_INCREMENT PRIMARY KEY,"+
-    "puesto VARCHAR(255) NOT NULL,"+
-    "descripcion TEXT,"+
-    "created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP)" 
-
-let crearTablaUsuarios = "CREATE TABLE IF NOT EXISTS usuarios ("+
-    "idUsuarios INT AUTO_INCREMENT PRIMARY KEY,"+
-    "Login VARCHAR(255) NOT NULL,"+
-    "nombre VARCHAR(255) NOT NULL,"+
-    "apellidoPaterno VARCHAR(255) NOT NULL,"+
-    "apellidoMaterno VARCHAR(255) NOT NULL,"+
-    "email VARCHAR(255) NOT NULL,"+
-    "puesto VARCHAR(255) NOT NULL,"+
-    "telefono VARCHAR(255) NOT NULL,"+
-    "created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP)" 
-
-let crearTablaPersonal = "CREATE TABLE IF NOT EXISTS personal ("+
-    "idPersonal INT AUTO_INCREMENT PRIMARY KEY,"+
-    "nombre VARCHAR(255) NOT NULL,"+
-    "apellidoMaterno VARCHAR(255) NOT NULL,"+
-    "apellidoPaterno VARCHAR(255) NOT NULL,"+
-    "estatusMigratorio VARCHAR(255) NOT NULL,"+
-    "esquemaPago VARCHAR(255) NOT NULL,"+
-    "formaPago VARCHAR(255) NOT NULL,"+
-    "created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP)" 
-    
-let crearTablaformaPago = "CREATE TABLE IF NOT EXISTS formaPago ("+
-    "idformaPago INT AUTO_INCREMENT PRIMARY KEY,"+
-    "description TEXT,"+
-    "created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP)" 
-
-let crearTablaPeriodoPago = "CREATE TABLE IF NOT EXISTS periodoPago ("+
-    "idperiodoPago INT AUTO_INCREMENT PRIMARY KEY,"+
-    "description TEXT,"+
-    "created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP)" 
-
-
-
-
-
-
-
-
-async function CrearTablas(){
-    let tabla1 = await  funciones.ConsultarQuery(crearTablaSucursal)
-    let tabla2 = await funciones.ConsultarQuery(crearTablaPuest)
-    let tabla3 = await funciones.ConsultarQuery(crearTablaUsuarios)
-    let tabla4 = await funciones.ConsultarQuery(crearTablaPersonal)
-    let tabla5 = await funciones.ConsultarQuery(crearTablaformaPago)
-    let tabla6 = await funciones.ConsultarQuery(crearTablaPeriodoPago)
-
-}
-CrearTablas()
-*/
-
-
-
-/* FIN DE CONEXION*/
-
-//Ruta Simple
 
 app.get("/", (req,res)=>{
     res.json({msg:"Bienvenido a la app de Pagos"})
@@ -142,6 +62,7 @@ app.post("/enviarData", (req,res)=>{
                     servicio_de_inters: req.query.zona,     
                     orgid: "Landing page faciales",
 
+                
                 }
         })
       };
@@ -188,14 +109,32 @@ app.post("/enviarData", (req,res)=>{
                   'Content-Type': 'application/json',
                   'Api-Token': 'c95cdd96d4e0f990c4e706f0dc519789031fd7dcb4a679d1c14bc3c647018e84592c1904'
                 },
+
                 body: JSON.stringify(
                     {            
                         contact:{
-                            fieldValues:[{
+                            fieldValues:[
+                                {
                                 field:"36",
                                 value: req.query.zona,
                                 contact: idContact,
-                            }]
+                                },
+                                {
+                                    field:"30",
+                                    value: req.query.source,
+                                    contact: idContact,
+                                },
+                                {
+                                    field:"31",
+                                    value: req.query.medium,
+                                    contact: idContact,                                
+                                },
+                                {
+                                    field:"32",
+                                    value: req.query.campaign,
+                                    contact: idContact,
+                                }
+                            ]
                         }            
                         
                 })
@@ -219,6 +158,33 @@ app.post("/enviarData", (req,res)=>{
 
 //Rutas
 app.use("/api", routes)
+
+
+
+
+
+cron.schedule('*/2 * * * *', () => {
+    /*
+    let hoy = moment().toDate()
+    console.log(hoy)
+    const optionsLista = {
+        method: 'GET',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+          'Api-Token': 'c95cdd96d4e0f990c4e706f0dc519789031fd7dcb4a679d1c14bc3c647018e84592c1904'
+        },
+      };
+    fetch('https://mxnycorp.api-us1.com/api/3/contacts?status=-1&filters[updated_after]='+hoy+'&orders[email]=ASC', optionsLista)
+    .then(response => response.json())
+    .then(response =>{
+        console.log(response.contacts.length)
+    })
+    .catch(err => console.error(err));
+    */
+});
+
+
 
 const PORT = 3000;
 
